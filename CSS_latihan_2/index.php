a2f98178d4f633a5f64e7c14405cfd6d9c09e1c1
adeh25 <adehkurniasari09@gmail.com>
2024-05-06 15:15:59 +0700

    latihan css 2

---
+++ CSS_latihan_2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Card</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-img">
+            <img src="https://i.pinimg.com/236x/1d/50/1c/1d501c79275d38ef3ec822e9d3960e85.jpg" alt="">
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">~ Spring Day ~</h2>
+                <p class="card-intro">Lagu Spring Day ini sendiri dipercaya merupakan sebuah karya untuk mengenang tragedi tenggelamnya Kapal Ferry Sewol tahun 2014 silam. 
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
